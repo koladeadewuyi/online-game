ddb3c75a5bee77a4b157533f1582ff7f5d16cb61
Kolade <koladeadewuyi@gmail.com>
2018-06-15 18:48:56 +0100

    Add RDD runner with tests

--- src/main/scala/com/db/exercise/RDDRunner.scala
+++ src/main/scala/com/db/exercise/RDDRunner.scala
@@ -1,25 +1,83 @@
 package com.db.exercise
 
+import java.io.{File, PrintWriter}
+import java.text.DecimalFormat
+
+import com.db.exercise.model._
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 
 class RDDRunner(val context: SparkContext) {
+
   /**
+    * Extracts input filePath into RDD[PlayerInfo] for each alias
     * @param input a map of the file paths to extract. The key is a [[String]] alias, the value is the [[String]] path of the file to extract.
     * @return a map of [[RDD]] per each input
     */
-  def extract(input: Map[String, String]): Map[String, RDD[AnyRef]] = ???
+  def extract(input: Map[String, String]): Map[String, RDD[PlayerInfo]] = {
+    input.map { case (alias, path) =>
+      alias -> context.textFile(path).map { line =>
+        val data = line.split(",").map(elem => elem.trim)
+        data match {
+          case Array(playerInfo, team) =>
+            PlayerTeam(playerInfo, team)
+          case Array(playerInfo, dayOfWeek, score) =>
+            PlayerScore(playerInfo, dayOfWeek, score.toFloat)
+          case unexpected =>
+            throw new IllegalArgumentException(s"File contained unexpected line $unexpected")
+        }
+      }
+    }
+  }
 
   /**
+    * Transforms extracted data into the result containing the team(s) and player(s) with highest scores
     * @param extracted a map of [[RDD]] indexed by a [[String]] alias
     * @return
     */
-  def transform(extracted: Map[String, RDD[AnyRef]]): RDD[AnyRef] = ???
+  def transform(extracted: Map[String, RDD[PlayerInfo]]): RDD[Winner] = {
+    val playerTeamsRDD = extracted(TeamsAlias).asInstanceOf[RDD[PlayerTeam]].keyBy(_.playerInfo)
+    val playerScoresRDD = extracted(ScoresAlias).asInstanceOf[RDD[PlayerScore]].keyBy(_.playerInfo)
+
+    val teamPlayerScores = playerTeamsRDD.join(playerScoresRDD).values.map { case (playerTeam, playerScore) =>
+      (playerTeam.team, playerScore.playerInfo, playerScore.score)
+    }.filter { case (_, _, score) =>
+      score <= 10 && score >= 0
+    }
+
+    val groupedByTeam = teamPlayerScores.groupBy { case (team, _, _) => team }
+    val teamScores = computeTotalScore(groupedByTeam)
+
+    val groupedBuPlayer = teamPlayerScores.groupBy { case (_, player, _) => player }
+    val playerScores = computeTotalScore(groupedBuPlayer)
+
+    val winners = Seq(teamScores, playerScores)
+      .flatMap(_.distinct.groupBy(_._2).max._2.toSeq)
+      .map { case (name, score) => Winner(name, score) }
+
+    context.parallelize(winners)
+  }
 
   /**
+    * Persists the transformed results to destination file path
     * @param transformed the [[RDD]] to store as a file
-    * @param path the path to save the output file
+    * @param path        the path to save the output file
     */
-  def load(transformed: RDD[AnyRef], path: String): Unit = ???
+  def load(transformed: RDD[Winner], path: String): Unit = {
+    val scoreFormat = new DecimalFormat("#.00")
+    val winners = transformed.collect
+    val writer = new PrintWriter(new File(path))
+    try {
+      winners.foreach { winner =>
+        writer.append(s"${winner.names} ${scoreFormat.format(winner.totalScore)}${System.lineSeparator}")
+      }
+    } finally writer.close()
+  }
+
+  private def computeTotalScore(input: RDD[(String, Iterable[(String, String, Float)])]) = {
+    input.map { case (team, teamPlayers) =>
+      (team, teamPlayers.map(_._3).sum)
+    }
+  }
 
 }
--- src/test/scala/com/db/exercise/RDDRunnerSpec.scala
+++ src/test/scala/com/db/exercise/RDDRunnerSpec.scala
@@ -1,24 +1,24 @@
 package com.db.exercise
 
-import com.db.exercise.model.{PlayerScore, PlayerTeam}
+import com.db.exercise.model._
 
 class RDDRunnerSpec extends TestFixture {
 
-  describe("RDDRunnerSpec") {
+  describe("RDDRunner") {
 
     describe("extract") {
       it("return a map of alias -> RDD[PlayerInfo] when given a map of alias -> filePath") {
         withSpark { sparkSession =>
-          val teamsFilePath = getClass.getResource("/teams.dat").getPath
-          val scoresFilePath = getClass.getResource("/scores.dat").getPath
-          val input = Map("TEAMS" -> teamsFilePath, "SCORES" -> scoresFilePath)
+          val teamsFilePath = getClass.getResource("/happyCase/teams.dat").getPath
+          val scoresFilePath = getClass.getResource("/happyCase//scores.dat").getPath
+          val input = Map(TeamsAlias -> teamsFilePath, ScoresAlias -> scoresFilePath)
 
           val expectedPlayerScores = Seq(
-            PlayerScore("PLAYER1", "DAY1", 8.95),
-            PlayerScore("PLAYER2", "DAY1", 10.00),
-            PlayerScore("PLAYER3", "DAY1", 7.30),
-            PlayerScore("PLAYER1", "DAY2", 2.35),
-            PlayerScore("PLAYER3", "DAY2", 3.35)
+            PlayerScore("PLAYER1", "DAY1", 8.95F),
+            PlayerScore("PLAYER2", "DAY1", 10.00F),
+            PlayerScore("PLAYER3", "DAY1", 7.30F),
+            PlayerScore("PLAYER1", "DAY2", 2.35F),
+            PlayerScore("PLAYER3", "DAY2", 3.35F)
           )
           val expectedPlayerTeams = Seq(
             PlayerTeam("PLAYER1", "TEAM1"),
@@ -28,23 +28,60 @@ class RDDRunnerSpec extends TestFixture {
 
           val extractionResult = new RDDRunner(sparkSession.sparkContext).extract(input)
 
-          extractionResult("TEAMS").collect().toSeq should contain theSameElementsAs expectedPlayerTeams
-          extractionResult("SCORES").collect().toSeq should contain theSameElementsAs expectedPlayerScores
+          extractionResult(TeamsAlias).collect().toSeq should contain theSameElementsAs expectedPlayerTeams
+          extractionResult(ScoresAlias).collect().toSeq should contain theSameElementsAs expectedPlayerScores
         }
       }
     }
 
     describe("transform") {
-      it("should transform") {
+      it("should transform TEAMS and SCORES into winning team(s) and winning players(s)") {
+        withSpark { sparkSession =>
+          val teams: Seq[PlayerInfo] = Seq(
+            PlayerTeam("PLAYER1", "TEAM1"),
+            PlayerTeam("PLAYER2", "TEAM2"),
+            PlayerTeam("PLAYER3", "TEAM1"),
+            PlayerTeam("PLAYER4", "TEAM2"),
+            PlayerTeam("PLAYER5", "TEAM3"),
+            PlayerTeam("PLAYER6", "TEAM3")
+          )
+          val scores: Seq[PlayerInfo] = Seq(
+            PlayerScore("PLAYER1", "DAY1", 1.2F),
+            PlayerScore("PLAYER2", "DAY1", 1.2F),
+            PlayerScore("PLAYER1", "DAY2", 2.3F),
+            PlayerScore("PLAYER4", "DAY2", 2.7F),
+            PlayerScore("PLAYER5", "DAY3", 2.5F),
+            PlayerScore("PLAYER6", "DAY4", 1.4F)
+          )
+          val sparkContext = sparkSession.sparkContext
+          val extracted = Map(
+            TeamsAlias -> sparkContext.parallelize(teams),
+            ScoresAlias -> sparkContext.parallelize(scores)
+          )
+          val expectedWinners = Seq(Winner("TEAM2", 3.9F), Winner("TEAM3", 3.9F), Winner("PLAYER1", 3.5F))
 
+          val transformedData = new RDDRunner(sparkContext).transform(extracted)
+
+          import sparkSession.implicits._
+          transformedData.toDS.as[Winner].collect should contain theSameElementsAs expectedWinners
+        }
       }
     }
 
     describe("load") {
-      it("should load") {
+      it("should load RDD[Winner] to specified filePath limiting scores to two decimal places") {
+        withSpark { sparkSession =>
+          val outputFilePath = s"${getClass.getResource("/rdd_out").getPath}/${getClass.getSimpleName}"
+          val winner = Winner("TEAM1", 11.12345F)
+          val sparkContext = sparkSession.sparkContext
+          val transformed = sparkContext.parallelize(Seq(winner))
+          val expectedWinner = Winner("TEAM1", 11.12F)
 
+          new RDDRunner(sparkContext).load(transformed, outputFilePath)
+
+          contentsOfFile(outputFilePath) should contain theSameElementsAs Seq(expectedWinner)
+        }
       }
     }
-
   }
 }
