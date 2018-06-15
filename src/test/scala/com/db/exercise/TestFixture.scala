42fc6d12ee7e942b1b594f3199f399d0a967e0f4
Kolade <koladeadewuyi@gmail.com>
2018-06-15 18:48:19 +0100

    Add test fixture

--- src/test/scala/com/db/exercise/TestFixture.scala
+++ src/test/scala/com/db/exercise/TestFixture.scala
@@ -1,21 +1,24 @@
 package com.db.exercise
 
-import com.db.exercise.model.{PlayerInfo, PlayerScore, PlayerTeam}
+import java.text.DecimalFormat
+
+import com.db.exercise.model.Winner
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import org.scalatest.{FunSpec, Matchers}
+
+import scala.io.Source
 
 trait TestFixture extends FunSpec with Matchers {
   private val appName = getClass.getSimpleName
   private val master = "local[*]"
   private val sparkConf = new SparkConf().setAppName(appName)
     .setMaster(master)
-    .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-    .registerKryoClasses(Array(classOf[PlayerTeam], classOf[PlayerScore], classOf[PlayerInfo]))
 
-  private val sparkSession = SparkSession.builder().config(sparkConf).getOrCreate()
+  val scoreFormat = new DecimalFormat("#.00")
 
   def withSpark(test: SparkSession => Unit) {
+    val sparkSession = SparkSession.builder().config(sparkConf).getOrCreate()
     try {
       test(sparkSession)
     } finally {
@@ -23,4 +26,20 @@ trait TestFixture extends FunSpec with Matchers {
       sparkSession.sparkContext.stop()
     }
   }
+
+  def contentsOfFile(filePath: String): Seq[Winner] = {
+    val source = Source.fromFile(filePath)
+    try {
+      source.getLines.map { line =>
+        val winner = line.split(" ").map(_.trim)
+        val winnerId = winner(0)
+        val score = winner(1).toFloat
+        Winner(winnerId, score)
+      }.toList
+    } finally{
+      source.close()
+    }
+  }
+
+  def getResourcePath(resource: String): String = getClass.getResource(resource).getPath
 }
